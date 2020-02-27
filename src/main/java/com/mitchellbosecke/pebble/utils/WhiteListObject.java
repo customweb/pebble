2598fb1599c9df2e45ed18ff3b57ede2a45e0f48
Jason Burley <jason.burley@wallee.com>
2020-02-27 12:56:47 +0100

    Jabu-WAL-5518 : WhiteList Object Update.

--- src/main/java/com/mitchellbosecke/pebble/utils/WhiteListObject.java
+++ src/main/java/com/mitchellbosecke/pebble/utils/WhiteListObject.java
@@ -2,17 +2,26 @@ package com.mitchellbosecke.pebble.utils;
 
 import java.lang.reflect.Method;
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 public class WhiteListObject {
 
-    private final Set<Method> whiteList;
-
+    private final Set<Method> mutedWhitelist;
     public Set<Method> getWhiteList() {
-        return whiteList;
+        return mutedWhitelist;
+    }
+    // Future Scope:
+    // public WhiteListObject setNewWhiteList(Set<Method> newWhiteList) { return new WhiteListObject(newWhiteList); }
+
+    // Constructor
+    public WhiteListObject(Set<Method> whiteList) {
+        // Future Scope:
+        // String nullResponseMessage = "WhiteList must not be null"; // Exception optional
+        //Set<Method> nonNullSetList = new HashSet<>(Objects.requireNonNull(whiteList, nullResponseMessage));
+        Set<Method> nonNullSetList = new HashSet<>(Objects.requireNonNull(whiteList));
+        this.mutedWhitelist = Collections.unmodifiableSet(nonNullSetList);
     }
 
-    public WhiteListObject(Set<Method> whiteList) {
-        this.whiteList = Collections.unmodifiableSet(whiteList);
-    }
 }
